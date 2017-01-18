cf95db5d74dc0e9ab804900b369529e3d7fe1b70
Owen LI <oli4@ds2.CS.McGill.CA>
2017-01-18 14:23:35 -0500

    regression calcs

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import math
 
 
 
@@ -18,15 +19,47 @@ with open('attendance.csv','rb') as csvfile:
 X = X[1:28]
 Y = Y[1:28]
 
-X = np.array(X)
-Y = np.array(Y)
-W = np.zeros(4)
-
-print W
+X = np.matrix(X)
+Y = np.matrix(Y)
+W = np.zeros((4,1))
 
 
-def logistic_func(weights, x):
-    return float(1) / (1 + math.e**(-x.dot(weights)))
+X = X.astype(np.float32)
+Y = Y.astype(np.float32)
+W = W.astype(np.float32)
 
+def logistic_func(W, x):
+	wt = np.transpose(W)
+	x = np.transpose(x)
+	dot = np.dot(wt,x)
+	print (1) / (1 + math.pow(math.e,-dot))
 
+def error_func(W, X, Y):
+	errorSumP1 = 0
+	errorSumP2 = 0
+	totalError = 0
+	for(x,y) in zip(X,Y):
+		sigmoid = logistic_func(W,x)
+    	errorSumP1 = y * math.log(sigmoid)
+    	errorSumP2 = (1-y) * math.log(1 - sigmoid)
+    	totalError = totalError + errorSumP1 + errorSumP2
+    return totalError
 
+def gradient_desc(weights, X, Y, alpha=.001, converge_change=.001):
+    #normalize
+    #X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    #setup cost iter
+    error = error_func(weights, X, Y)
+    change_cost = 1
+    while(change_cost > converge_change):
+        old_error = error
+        weights = weights - (alpha * log_gradient(theta_values, X, y))
+        cost = cost_func(theta_values, X, y)
+        change_cost = old_cost - cost
+    return weights
+
+def gradient(weights, X, Y):
+	for(x,y) in zip(X,Y):
+		sigmoid = logistic_func(weights, x) - np.squeeze(y)
+		final_calc = first_calc.T.dot(x)
+		return final_calc
