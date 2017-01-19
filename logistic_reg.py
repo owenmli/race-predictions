2ee17fb1212b7909c6d914fd02b4fa0925688160
Owen LI <oli4@ds2.CS.McGill.CA>
2017-01-19 18:16:37 -0500

    done?

--- logistic_reg.py
+++ logistic_reg.py
@@ -3,6 +3,7 @@ import csv
 import math
 from decimal import *
 import matplotlib.pyplot as plt
+import random
 
 
 
@@ -26,6 +27,9 @@ Y = np.matrix(Y)
 W = np.zeros(5)
 W = np.matrix(W)
 W = np.transpose(W)
+
+
+
 
 
 X = X.astype(np.float)
@@ -55,32 +59,21 @@ def error_func(W, X, Y):
 		totalError += (np.asscalar(y) * math.log(sigmoid)) + ((1.0-np.asscalar(y)) * math.log(1.0 - sigmoid))
 	return -totalError
 
-def gradient_desc(W, X, Y, alpha=.000015, converge_change= 0.5):
-    #normalize
-    X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
-    X = np.c_[np.ones(X.shape[0]),X]
-    cost_iter = []
-    error = error_func(W, X, Y)
-    cost_iter.append([0, error])
-    change_cost = 1
-    i=0
-    while(change_cost > converge_change):
-        old_error = error
-        W = W + (alpha * (gradient(W, X, Y)))
-        error = error_func(W, X, Y)
-        cost_iter.append([i, error])
-        change_cost = old_error - error
-    	print "Iteration: " + str(i) + " Delta: " + str(change_cost) + " Error: " + str(error)
-        i+=1
+def gradient_desc(W, X, Y, alpha=.00015, converge_change= 0.5):
+	X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+	X = np.c_[np.ones(X.shape[0]),X]
+	error = error_func(W, X, Y)
+	change_cost = 1
+	i=0
+	while(change_cost > converge_change):
+		old_error = error
+		W = W + (alpha * (gradient(W, X, Y)))
+		error = error_func(W, X, Y)
+		change_cost = old_error - error
+		print "Iteration: " + str(i) + " Delta: " + str(change_cost) + " Error: " + str(error)
+		i+=1
 
-	# plt.plot(cost_iter[:,0], cost_iter[:,1])
-	# plt.ylabel("Cost")
-	# plt.xlabel("Iteration")
-	# sns.despine()
-
-
-
-    return W
+	return W
 
 def gradient(W, X, Y):
 
@@ -98,7 +91,48 @@ def gradient(W, X, Y):
 
 	return sumOfVectors
 
-W = gradient_desc(W,X,Y)
-print W
+def calcAccuracy(W,X,Y):
+	counter = 0
+	X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+	X = np.c_[np.ones(X.shape[0]),X]
+	WT = np.transpose(W)
+	for (xi,y) in zip(X,Y):
+		result = logistic_func(WT,xi)
+		print result * 100
+		if (result > 0.5 and y == 1) or (result <= 0.5 and y == 0):
+			counter += 1
 
+
+	successrate = 100 * (float(counter)/float(len(X)))
+	print "Accuracy Rate: " + str(successrate) + "%"
+
+
+# Wtest = np.zeros(5)
+# Wtest = np.matrix(Wtest)
+# Wtest = np.transpose(Wtest)
+
+
+def generateNFoldWeights(n, X, Y, W):
+
+	combined = list(zip(X,Y))
+	random.shuffle(combined)
+	X[:], Y[:] = zip(*combined)
+
+	nlength = float(len(X))/n
+	nX = X[0:int(nlength)]
+	nY = Y[0:int(nlength)]
+	tX = X[int(nlength)+1:]
+	tY = Y[int(nlength)+1:]
+
+	W = gradient_desc(W,nX,nY)
+	calcAccuracy(W,tX,tY)
+	#calcAccuracy(W, nX, nY)
+
+	# W = gradient_desc(W,nX, nY)
+	# calcAccuracy(W, nX,nY)
+	# calcAccuracy(W, tX, tY)
+
+
+
+generateNFoldWeights(2,X,Y,W)
 
